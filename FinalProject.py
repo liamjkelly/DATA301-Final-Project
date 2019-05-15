c6bc8576a613ff65755789b96f208667634af902
liamjkelly <liamjkelly11@gmail.com>
2019-05-15 16:45:01 +1200

    Wrote function to map Goldstein Scores

--- FinalProject.py
+++ FinalProject.py
@@ -2,9 +2,11 @@
 !wget -q http://archive.apache.org/dist/spark/spark-2.3.2/spark-2.3.2-bin-hadoop2.7.tgz
 !tar xf spark-2.3.2-bin-hadoop2.7.tgz
 !pip install -q pyspark
+!pip install gdelt
 
 import pyspark
 import gdelt
+gd2 = gdelt.gdelt(version=2)
 from pyspark import SparkConf, SparkContext
 conf = SparkConf().setMaster("local[*]").set("spark.executor.memory", "1g")
 sc = SparkContext(conf = conf)
@@ -19,3 +21,18 @@ def dbg(x):
            for t in x.take(100)])
   else:
     print(x)
+
+results = gd2.Search(['2016 11 01'], table='events', coverage=True)
+goldstein_list = sc.parallelize(results['GoldsteinScale'].tolist())
+def map_Goldstein(x):
+  if x < -5:
+    return -2
+  elif x < 0:
+    return -1
+  elif x > 5:
+    return 2
+  elif x >= 0:
+    return 1
+  
+goldstein_mapped = goldstein_list.map(map_Goldstein)
+dbg(goldstein_mapped.take(60))
